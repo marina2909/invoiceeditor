b7ab2719ce7afd5c712e5d432d8c64874e9bc83b
Marina Sovic <sovic.marina@gmail.com>
2017-03-16 17:07:56 -0700

    Defining buld process and initial structure

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Application extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			invoiceitems: []
+		};
+	}
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+
+
